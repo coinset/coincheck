4f43f3b759b448bcfed0864687d6b1157234ccf6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-02 22:14:54 +0900

    chore: output esm module

--- packages/coincheck-client/rollup.config.ts
+++ packages/coincheck-client/rollup.config.ts
@@ -1,4 +1,4 @@
-import { main } from './package.json'
+import { main, module } from './package.json'
 import alias from '@rollup/plugin-alias'
 import { join } from 'path'
 import typescript from 'rollup-plugin-typescript2'
@@ -6,10 +6,14 @@ import commonjs from 'rollup-plugin-commonjs'
 const config = {
   input: 'src/index.ts',
 
-  output: {
-    file: main,
-    format: 'cjs'
-  },
+  output: [
+    {
+      file: main,
+      format: 'cjs'
+    },
+
+    { file: module, format: 'es' }
+  ],
 
   plugins: [
     alias({
