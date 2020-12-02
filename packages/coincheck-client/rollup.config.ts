a0c58eef99a7900ea03d13869cd18b331c9536af
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-12-03 00:47:24 +0900

    chore(coincheck-client): change rollup config

--- packages/coincheck-client/rollup.config.ts
+++ packages/coincheck-client/rollup.config.ts
@@ -3,6 +3,7 @@ import alias from '@rollup/plugin-alias'
 import { join } from 'path'
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
+import copy from 'rollup-plugin-copy'
 const config = {
   input: 'src/index.ts',
 
@@ -11,16 +12,31 @@ const config = {
       file: main,
       format: 'cjs'
     },
-
     { file: module, format: 'es' }
   ],
+  external: ['got', 'path'],
 
   plugins: [
     alias({
-      entries: [{ find: /^@\//, replacement: join(__dirname, 'src', '/') }]
+      entries: [{ find: /^@\//, replacement: join(__dirname, 'src/') }]
     }),
     commonjs(),
-    typescript()
+    typescript({
+      typescript: require('ttypescript'),
+      tsconfigDefaults: {
+        compilerOptions: {
+          plugins: [
+            {
+              transform: 'typescript-transform-paths',
+              afterDeclarations: true
+            }
+          ]
+        }
+      }
+    }),
+    copy({
+      targets: [{ src: 'src/types', dest: 'dist' }]
+    })
   ]
 }
 
